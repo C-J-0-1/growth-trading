ad59be6a550eb6895f4fef6119f46ecfa712631d
C-J-0-1 <79644606+C-J-0-1@users.noreply.github.com>
2023-09-29 19:38:28 +0530

    initial commit

---
+++ growth/src/test/java/com/assignment/growth/GrowthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.growth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
